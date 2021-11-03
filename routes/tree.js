c2e2af5d975a9fa37b6bcae218a541775337ba6c
vamsi <vamsibhogireddy735@gmail.com>
2021-11-03 13:40:23 -0500

    bonus3

--- routes/tree.js
+++ routes/tree.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("tree", { title: "Search Results Restaurant" });
+  res.render("tree", { title: "Search Results Tree" });
 });
 
 module.exports = router;
